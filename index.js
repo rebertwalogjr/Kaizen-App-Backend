4a19e066b7cf478c84ced8da2a02e5f285927c2b
rebertwalogjr <rebertwalogjr@gmail.com>
2022-09-28 23:52:03 +0800

    resolve cors credentials

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 const mongoose = require("mongoose");
 const NoteModel = require("./models/Notes");
 const TodoModel = require("./models/Todo");
-const uri = require("./constant-list")
+const uri = require("./constant-list");
 
 app.use(express.json());
+
 app.use(cors());
+app.options("*", cors())
 
 mongoose.connect(uri);
 
@@ -22,14 +24,14 @@ app.get("/getNotes", (req, res) => {
 });
 
 app.get("/getTodos", (req, res) => {
-    TodoModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    })
-})
+  TodoModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 app.post("/saveNote", async (req, res) => {
   const note = req.body;
@@ -39,26 +41,25 @@ app.post("/saveNote", async (req, res) => {
 });
 
 app.post("/saveTodo", async (req, res) => {
-    const todo = req.body;
-    const newTodo = new TodoModel(todo);
-    await newTodo.save();
-    res.json(todo);
-  });
+  const todo = req.body;
+  const newTodo = new TodoModel(todo);
+  await newTodo.save();
+  res.json(todo);
+});
 
 app.post("/deleteNote", (req, res) => {
-    NoteModel.findOneAndDelete({ id: req.body.id}, (err, response) => {
-        if (err) return console.log(err);
-        res.json(response);
-    })
+  NoteModel.findOneAndDelete({ id: req.body.id }, (err, response) => {
+    if (err) return console.log(err);
+    res.json(response);
+  });
 });
 
 app.post("/deleteTodo", (req, res) => {
-  TodoModel.findOneAndDelete({ id: req.body.id}, (err, response) => {
-      if (err) return console.log(err);
-      res.json(response);
-  })
+  TodoModel.findOneAndDelete({ id: req.body.id }, (err, response) => {
+    if (err) return console.log(err);
+    res.json(response);
+  });
 });
-
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("Sever running at port 3001");
